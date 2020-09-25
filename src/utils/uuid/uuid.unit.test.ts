2cb696de9c98760c1fb5b064c1133209166932b1
InteractiveTimmy <interactivetimmy@gmail.com>
2020-09-25 18:32:55 -0400

    feat(utils): create uuid generator

---
+++ src/utils/uuid/uuid.unit.test.ts
@@ -0,0 +1,21 @@
+import uuid from './uuid';
+
+describe('uuid#unit', () => {
+  it('should generate a string', () => {
+    expect.assertions(1);
+
+    expect(typeof uuid()).toStrictEqual('string');
+  });
+
+  it('should generate a string of 36 characters', () => {
+    expect.assertions(1);
+
+    expect(uuid()).toHaveLength(36);
+  });
+
+  it('should generate a unique string', () => {
+    expect.assertions(1);
+
+    expect(uuid()).not.toStrictEqual(uuid());
+  });
+});
