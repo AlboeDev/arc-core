535e19f63b88a2919a5206d25abe32c527dfe32e
InteractiveTimmy <interactivetimmy@gmail.com>
2020-09-27 18:34:29 -0400

    fix(models/arc-object): abstracting

---
+++ src/models/arc-object/arc-object.fixture.ts
@@ -0,0 +1,11 @@
+import ArcObject from './arc-object';
+
+/**
+ * ArcObjectFixture class.
+ *
+ * @remarks
+ * The testing fixture class for ArcObject.
+ *
+ * @internal
+ */
+export default class ArcObjectFixture extends ArcObject {}
