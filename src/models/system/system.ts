c1c9c33f029eac75cfdd3841ad4e847bf575084e
InteractiveTimmy <interactivetimmy@gmail.com>
2020-10-22 18:08:44 -0400

    docs(models/system): improve jsdoc documentation and testing

--- src/models/system/system.ts
+++ src/models/system/system.ts
@@ -1,4 +1,5 @@
 import ArcObject from '../arc-object/arc-object';
+import Entity from '../entity/entity';
 
 /**
  * System class.
@@ -7,6 +8,9 @@ import ArcObject from '../arc-object/arc-object';
  * The root system object for mutating Entities and Components.
  */
 export default abstract class System extends ArcObject {
+  /**
+   * The type of this System.
+   */
   public readonly type: string;
 
   /**
@@ -17,4 +21,11 @@ export default abstract class System extends ArcObject {
 
     this.type = this.constructor.name;
   }
+
+  /**
+   * Process a collection of Entities with this System.
+   *
+   * @param entities - Entities to process by this System.
+   */
+  public abstract process(delta: number, entities: Array<Entity>): void
 }
